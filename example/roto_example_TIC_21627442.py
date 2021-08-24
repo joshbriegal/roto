19ea879a5b349e6b0ac0e1dfc966cef7bf4830ac
GitHub Action <action@github.com>
2021-08-24 11:12:35 +0000

    fixup: Format Python code with Black

--- example/roto_example_TIC_21627442.py
+++ example/roto_example_TIC_21627442.py
@@ -9,20 +9,24 @@ if __name__ == "__main__":
         "example/TIC_21627442_2min_LC_sector_6.dat",
     )
 
-    #print("Imported the following light curve: ")
-    #print(tab)
+    # print("Imported the following light curve: ")
+    # print(tab)
 
-    mask = (tab["quality"].data==0)
+    mask = tab["quality"].data == 0
     timeseries = tab["time"].data[mask]
     flux = tab["flux"].data[mask]
     flux_errors = tab["flux_err"].data[mask]
 
-    roto = RoTo(timeseries, flux, flux_errors, 
-                methods_parameters={"lombscargle":{}, 
-                                    "fft":{}, 
-                                    "gacf":{}, 
-                                    "gp":{"gp_seed_period":3.853}
-                                    }
-        )
+    roto = RoTo(
+        timeseries,
+        flux,
+        flux_errors,
+        methods_parameters={
+            "lombscargle": {},
+            "fft": {},
+            "gacf": {},
+            "gp": {"gp_seed_period": 3.853},
+        },
+    )
     roto()
     print(roto)
