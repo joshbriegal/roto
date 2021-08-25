476d13087c12bce062567ade70de46812717676d
joshbriegal <josh@briegal.com>
2021-08-25 15:18:00 +0100

    Fix tests to allow new gp only specification

--- test/test_roto.py
+++ test/test_roto.py
@@ -13,13 +13,24 @@ inverse_methods_dictionary = {method: name for name, method in RoTo.METHODS.item
         {"lombscargle": {"normalization": True, "fit_mean": False}},
         {"gacf": {}},
         {"fft": {}},
-        {"gp": {}},
     ],
 )
 def test_create_roto(method_parameters, timeseries, flux, flux_errors):
     roto = RoTo(timeseries, flux, flux_errors, method_parameters)
 
-    for method in roto.methods:
+    for name, method in roto.methods.items():
         assert isinstance(method, PeriodFinder)
+        assert name in roto.METHODS.keys()
         if method_parameters:
             assert inverse_methods_dictionary[method.__class__] in method_parameters
+
+
+def test_create_roto_only_gp(timeseries, flux, flux_errors):
+    method_parameters = {"gp": {}}
+    roto = RoTo(timeseries, flux, flux_errors, method_parameters)
+
+    for name, method in roto.methods.items():
+        assert isinstance(method, PeriodFinder)
+        assert name in roto.METHODS.keys()
+
+    assert list(roto.methods.keys()) == ["lombscargle", "gp"]
