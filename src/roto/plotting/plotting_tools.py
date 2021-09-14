Add NGTS light curve example. Add logging to example scripts. Add masking to plotted data. Fix bug with phasing.

src/roto/plotting/plotting_tools.py
@@ -20,30 +20,32 @@ def split_phase(phase, data, timeseries=None, buff=0.9, nperiods=1):
     timeseriess = [] if timeseries is not None else None
 
     idx_changes = np.where(np.diff(phase) < 0)[0][::nperiods]
-    use_first = True if (phase[0] < 1.0 - buff) else False
-    use_last = True if (phase[-1] > buff) else False
+    if len(idx_changes) > 0:
+        use_first = True if (phase[0] < 1.0 - buff) else False
+        use_last = True if (phase[-1] > buff) else False
 
-    if use_first:
-        phases.append(phase[: idx_changes[0]])
-        datas.append(data[: idx_changes[0]])
-        if timeseriess is not None:
-            timeseriess.append(timeseries[: idx_changes[0]])
+        if use_first:
+            phases.append(phase[: idx_changes[0]])
+            datas.append(data[: idx_changes[0]])
+            if timeseriess is not None:
+                timeseriess.append(timeseries[: idx_changes[0]])
 
-    for i, idx in enumerate(idx_changes[:-1]):
-        phases.append(phase[idx + 1 : idx_changes[i + 1]])
-        datas.append(data[idx + 1 : idx_changes[i + 1]])
-        if timeseriess is not None:
-            timeseriess.append(timeseries[idx + 1 : idx_changes[i + 1]])
+        for i, idx in enumerate(idx_changes[:-1]):
+            phases.append(phase[idx + 1 : idx_changes[i + 1]])
+            datas.append(data[idx + 1 : idx_changes[i + 1]])
+            if timeseriess is not None:
+                timeseriess.append(timeseries[idx + 1 : idx_changes[i + 1]])
 
-    if use_last or np.any(np.diff(phase[idx_changes[-1] + 1 :]) < 0):
-        phases.append(phase[idx_changes[-1] :])
-        datas.append(data[idx_changes[-1] :])
+        if use_last or np.any(np.diff(phase[idx_changes[-1] + 1 :]) < 0):
+            phases.append(phase[idx_changes[-1] :])
+            datas.append(data[idx_changes[-1] :])
+            if timeseriess is not None:
+                timeseriess.append(timeseries[idx_changes[-1] :])
         if timeseriess is not None:
-            timeseriess.append(timeseries[idx_changes[-1] :])
-    if timeseriess is not None:
-        return phases, datas, timeseriess
-    else:
-        return phases, datas
+            return phases, datas, timeseriess
+        else:
+            return phases, datas
+    return [phase], [data]
 
 
 def calculate_phase(timeseries, period, epoch):

src/roto/roto.py
@@ -448,18 +448,44 @@ class RoTo:
         Returns:
             Axes: Matplotlib axis
         """
+
+        if "gp" in self.methods:
+            mask = self.methods['gp'].mask
+        else:
+            mask = np.ones(len(self.timeseries), dtype=bool)
+
         ax.errorbar(
-            self.timeseries,
-            self.flux,
-            self.flux_errors,
-            markersize=1,
-            errorevery=2,
-            linestyle=" ",
-            marker=".",
+            self.timeseries[mask],
+            self.flux[mask],
+            self.flux_errors[mask],
+            markersize=2,
+            errorevery=1,
+            linestyle="none",
+            marker="o",
             color="k",
-            capsize=1,
+            ecolor="gray",
+            alpha=0.7,
+            capsize=0,
             elinewidth=1,
+            mec='none'
         )
+
+        ax.errorbar(
+            self.timeseries[~mask],
+            self.flux[~mask],
+            self.flux_errors[~mask],
+            markersize=2,
+            errorevery=1,
+            linestyle="none",
+            marker="o",
+            color="k",
+            ecolor="gray",
+            alpha=0.3,
+            capsize=0,
+            elinewidth=1,
+            mec='none'
+        )
+
         ax.set_xlabel(f"Time / {self.time_units}")
         ax.set_ylabel(f"Flux / {self.flux_units}")
 